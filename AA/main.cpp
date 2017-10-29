514ef8d1e4c44e10d63f0801db317b90d7421e92
wodomierz <miron.ficak@gmail.com>
2017-10-29 10:58:33 +0100

    Initial Commit

---
+++ AA/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  AA
+//
+//  Created by Miron Ficak on 29/10/2017.
+//  Copyright © 2017 Miron Ficak. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
